0bf275863e3d8c4f8ceb72f9fd1f5b34ba6a8686
Gustavo Maciel <79233238+Gus-1003@users.noreply.github.com>
2022-06-19 17:28:30 -0300

    atalho de concatenar +1

--- Práticas/primo.py
+++ Práticas/primo.py
@@ -5,8 +5,9 @@ div = 0
 for x in range(1, a+1):
   resto = a % x
   if resto == 0:
-    div = div + 1
-
+    div = div + 1 
+    #div += 1
+    
 if div == 2:
   print('número {} é primo'.format(a))
 else:
